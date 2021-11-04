2dfa101cbc933abb19350b94e4f08092c2ff0b62
Bárbara Ann Lima <babiplima@gmail.com>
2021-11-04 11:44:57 -0300

    Criar métodos construtores

--- Calculo-investimento/src/main/java/DTO/InvestidorDTO.java
+++ Calculo-investimento/src/main/java/DTO/InvestidorDTO.java
@@ -2,12 +2,24 @@ package DTO;
 
 public class InvestidorDTO {
 
+    //Atributos
+
     private String email;
     private String nome;
     private String cpf;
     private int periodoDeAplicacaoMeses;
     private double valorPrevisto;
 
+    //Métodos construtores
 
+    public InvestidorDTO() {
+    }
 
+    public InvestidorDTO(String email, String nome, String cpf, int periodoDeAplicacaoMeses, double valorPrevisto) {
+        this.email = email;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.periodoDeAplicacaoMeses = periodoDeAplicacaoMeses;
+        this.valorPrevisto = valorPrevisto;
+    }
 }
