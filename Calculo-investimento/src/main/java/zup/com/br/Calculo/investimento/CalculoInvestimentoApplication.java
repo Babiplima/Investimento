0c9b2cddff4aaafff95105962610bd58c95c305c
Bárbara Ann Lima <babiplima@gmail.com>
2021-11-04 11:19:31 -0300

    Primeiro Commit

---
+++ Calculo-investimento/src/main/java/zup/com/br/Calculo/investimento/CalculoInvestimentoApplication.java
@@ -0,0 +1,13 @@
+package zup.com.br.Calculo.investimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculoInvestimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculoInvestimentoApplication.class, args);
+	}
+
+}
---
+++ Calculo-investimento/src/test/java/zup/com/br/Calculo/investimento/CalculoInvestimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package zup.com.br.Calculo.investimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculoInvestimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
